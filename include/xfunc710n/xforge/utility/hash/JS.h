6967d148ad46864908ce7a3a074af344ca73f811
Anton Kostserau <xfunc710n@gmail.com>
2026-01-17 11:22:43 +0300

    Hash JS Header

---
+++ include/xfunc710n/xforge/utility/hash/JS.h
@@ -0,0 +1,12 @@
+#ifndef xfunc710n_xforge_utility_hash_JS
+#define xfunc710n_xforge_utility_hash_JS
+
+#include "xfunc710n/xforge/xforge.h"
+
+UInt32
+hash_JS(
+  UInt8 *payload,
+  USize size
+);
+
+#endif
