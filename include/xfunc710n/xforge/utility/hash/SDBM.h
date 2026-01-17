9cdcf768be7512e76aac48a9ab0652dcd124f3a7
Anton Kostserau <xfunc710n@gmail.com>
2026-01-17 11:24:29 +0300

    Hash SDBM Header

---
+++ include/xfunc710n/xforge/utility/hash/SDBM.h
@@ -0,0 +1,12 @@
+#ifndef xfunc710n_xforge_utility_hash_SDBM
+#define xfunc710n_xforge_utility_hash_SDBM
+
+#include "xfunc710n/xforge/xforge.h"
+
+UInt32
+hash_SDBM(
+  UInt8 *payload,
+  USize size
+);
+
+#endif
