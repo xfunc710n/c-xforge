7368b9be6076fc500e5a8b241916e0867750f479
Anton Kostserau <xfunc710n@gmail.com>
2026-01-17 11:23:09 +0300

    Hash PJW Header

---
+++ include/xfunc710n/xforge/utility/hash/PJW.h
@@ -0,0 +1,12 @@
+#ifndef xfunc710n_xforge_utility_hash_PJW
+#define xfunc710n_xforge_utility_hash_PJW
+
+#include "xfunc710n/xforge/xforge.h"
+
+UInt32
+hash_PJW(
+  UInt8 *payload,
+  USize size
+);
+
+#endif
