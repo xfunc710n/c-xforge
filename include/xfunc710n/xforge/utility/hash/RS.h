2cbe19e36ca69ac9c5d87e302e34be87c253738f
Anton Kostserau <xfunc710n@gmail.com>
2026-01-17 11:23:44 +0300

    Hash RS Header

---
+++ include/xfunc710n/xforge/utility/hash/RS.h
@@ -0,0 +1,12 @@
+#ifndef xfunc710n_xforge_utility_hash_RS
+#define xfunc710n_xforge_utility_hash_RS
+
+#include "xfunc710n/xforge/xforge.h"
+
+UInt32
+hash_RS(
+  UInt8 *payload,
+  USize size
+);
+
+#endif
