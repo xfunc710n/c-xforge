69b219165bb3459e485d60692bdc3fd97a85bf75
Anton Kostserau <xfunc710n@gmail.com>
2026-01-17 11:22:11 +0300

    Hash ELF Header

---
+++ include/xfunc710n/xforge/utility/hash/ELF.h
@@ -0,0 +1,12 @@
+#ifndef xfunc710n_xforge_utility_hash_ELF
+#define xfunc710n_xforge_utility_hash_ELF
+
+#include "xfunc710n/xforge/xforge.h"
+
+UInt32
+hash_ELF(
+  UInt8 *payload,
+  USize size
+);
+
+#endif
