e8a2a14ee77b3533713b9eaa5c3e6562f3c41790
Anton Kostserau <xfunc710n@gmail.com>
2026-01-17 11:16:55 +0300

    Hash AP Header

---
+++ include/xfunc710n/xforge/utility/hash/AP.h
@@ -0,0 +1,12 @@
+#ifndef xfunc710n_xforge_utility_hash_AP
+#define xfunc710n_xforge_utility_hash_AP
+
+#include "xfunc710n/xforge/xforge.h"
+
+UInt32
+hash_AP(
+  UInt8 *payload,
+  USize size
+);
+
+#endif
