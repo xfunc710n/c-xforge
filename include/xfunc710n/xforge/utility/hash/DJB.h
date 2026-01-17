d6afdc4b8043eae29c5578ce09ac0e130dc6a738
Anton Kostserau <xfunc710n@gmail.com>
2026-01-17 11:21:29 +0300

    Hash DJB Header

---
+++ include/xfunc710n/xforge/utility/hash/DJB.h
@@ -0,0 +1,12 @@
+#ifndef xfunc710n_xforge_utility_hash_DJB
+#define xfunc710n_xforge_utility_hash_DJB
+
+#include "xfunc710n/xforge/xforge.h"
+
+UInt32
+hash_DJB(
+  UInt8 *payload,
+  USize size
+);
+
+#endif
